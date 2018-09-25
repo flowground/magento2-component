8cabde29f6f8cfe47e034a85afd6c9264dbb7e62
Olha Virolainen <olga.shukel@gmail.com>
2018-09-25 16:48:13 +0300

    Change authentication method

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,52 +1,38 @@
 const axios = require('axios');
+const {getToken} = require('./lib/helpers');
+
 
 module.exports = verify;
 
 function verify(credentials, callback) {
     (async () => {
-        let result;
-        let token;
-        let username = credentials.username;
-        let password = credentials.password;
-        let integrationToken = credentials.integrationToken;
-        if (!(username && password && integrationToken)) {
+        if (!(credentials.username && credentials.password && credentials.integrationToken)) {
             try {
-                if (username && password) {
-                    result = await axios.post(
-                        `${credentials.url}/rest/V1/integration/admin/token`, {
-                            username: username,
-                            password: password
+                let token = await getToken(credentials);
+                if (token !== null) {
+                    const baseUrl = `${credentials.url}/rest/all/V1`;
+
+                    const service = await axios.create({
+                        baseUrl,
+                        validateStatus: status => [200, 201, 404].includes(status),
+                        headers: {
+                            Authorization: `Bearer ${token}`
                         }
-                    );
-                    console.log('result.data', result.data);
+                    });
 
-                    if (!result || !result.data) {
-                        return callback(new Error('creds are not valid'));
-                    }
-                    token = result.data;
-                } else if (integrationToken) {
-                    token = integrationToken;
+                    const response = await service.get(`${baseUrl}/customers/search?searchCriteria[pageSize]=1`);
+                    console.log('Status Text', response.statusText);
+                    callback(null, response.statusText === 'OK');
+                } else {
+                    console.error('Credentials are not valid');
+                    return callback(new Error('Credentials are not valid'));
                 }
-                // testing api url, trying to retrieve a list of customers
-                const baseUrl = `${credentials.url}/rest/all/V1`;
-
-                const service = await axios.create({
-                    baseUrl,
-                    validateStatus: status => [200, 201, 404].includes(status),
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                });
-
-                const response = await service.get(`${baseUrl}/customers/search?searchCriteria[pageSize]=1`);
-
-                callback(null, response.statusText === 'OK');
             } catch (e) {
                 console.error(e);
-
                 callback(e);
             }
         } else {
+            console.error('You need to use one of options: Username and Password or Integration Token');
             return callback(new Error('You need to use one of options: Username and Password or Integration Token'));
         }
     })();
