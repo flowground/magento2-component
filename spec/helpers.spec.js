18f1565c2bd80d9271dc7eaf245517774d54f156
Olha Virolainen <olga.shukel@gmail.com>
2018-10-03 09:56:18 +0300

    Change helpers unit test

--- spec/helpers.spec.js
+++ spec/helpers.spec.js
@@ -59,24 +59,6 @@ describe('Unit test for helpers class methods', function () {
         });
     });
 
-    describe('Test getToken with not valid username and password', () => {
-        it('Set not valid username and password to config', async () => {
-            nock(url)
-                .post('/rest/V1/integration/admin/token', {
-                    username,
-                    password
-                })
-                .reply(404, 'Failed with status code 404');
-            cfg = {
-                url,
-                username,
-                password
-            };
-            const result = await getToken(cfg);
-            expect(result).to.deep.eq(null);
-        });
-    });
-
     describe('Test getBaseUrlAndService with Store in message', () => {
         it('Set not valid username and password to config', async () => {
             cfg = {
@@ -117,6 +99,7 @@ describe('Unit test for helpers class methods', function () {
             expect(result.baseUrl).to.deep.eq(url + '/rest/V1');
         });
     });
+
     describe('Test getStoreCodes', () => {
         it('First arg need to be "all"', async () => {
             nock(url)
@@ -137,6 +120,7 @@ describe('Unit test for helpers class methods', function () {
             expect(result).to.deep.eq(['all', 'default', 'admin']);
         });
     });
+
     describe('Test getStoreCodeSelectModel', () => {
         it('First arg need to be "all"', async () => {
             nock(url)
